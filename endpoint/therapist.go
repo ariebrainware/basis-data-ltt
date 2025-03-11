879fa379648c1a7f603eacdc28f64b9555c51ba2
Arie Brainware <publicu001@protonmail.com>
2025-03-11 15:13:47 +0700

    feat: add validation and update logic for therapist creation and modification

--- endpoint/therapist.go
+++ endpoint/therapist.go
@@ -13,7 +13,7 @@ import (
 type createTherapistRequest struct {
 	FullName    string `json:"full_name" binding:"required"`
 	Email       string `json:"email" binding:"required"`
-	Password    string `json:"password" binding:"required"`
+	Password    string `json:"password"`
 	PhoneNumber string `json:"phone_number" binding:"required"`
 	Address     string `json:"address" binding:"required"`
 	DateOfBirth string `json:"date_of_birth" binding:"required"`
@@ -22,48 +22,6 @@ type createTherapistRequest struct {
 	Height      int    `json:"height" binding:"required"`
 	Role        string `json:"role" binding:"required"`
 	IsApproved  bool   `json:"is_approved"`
-}
-
-func CreateTherapist(c *gin.Context) {
-	therapistRequest := createTherapistRequest{}
-
-	if err := c.ShouldBindJSON(&therapistRequest); err != nil {
-		util.CallUserError(c, util.APIErrorParams{
-			Msg: "Invalid request body",
-			Err: err,
-		})
-		return
-	}
-
-	if err := validateTherapistRequest(therapistRequest); err != nil {
-		util.CallUserError(c, util.APIErrorParams{
-			Msg: err.Error(),
-			Err: fmt.Errorf("invalid payload"),
-		})
-		return
-	}
-
-	db, err := config.ConnectMySQL()
-	if err != nil {
-		util.CallServerError(c, util.APIErrorParams{
-			Msg: "Failed to connect to MySQL",
-			Err: err,
-		})
-		return
-	}
-
-	if err := createTherapistInDB(db, therapistRequest); err != nil {
-		util.CallServerError(c, util.APIErrorParams{
-			Msg: "Failed to create therapist",
-			Err: err,
-		})
-		return
-	}
-
-	util.CallSuccessOK(c, util.APISuccessParams{
-		Msg:  "Therapist created",
-		Data: nil,
-	})
 }
 
 func validateTherapistRequest(req createTherapistRequest) error {
@@ -111,5 +69,98 @@ func createTherapistInDB(db *gorm.DB, req createTherapistRequest) error {
 		}
 
 		return nil
+	})
+}
+
+func CreateTherapist(c *gin.Context) {
+	therapistRequest := createTherapistRequest{}
+
+	if err := c.ShouldBindJSON(&therapistRequest); err != nil {
+		util.CallUserError(c, util.APIErrorParams{
+			Msg: "Invalid request body",
+			Err: err,
+		})
+		return
+	}
+
+	if err := validateTherapistRequest(therapistRequest); err != nil {
+		util.CallUserError(c, util.APIErrorParams{
+			Msg: err.Error(),
+			Err: fmt.Errorf("invalid payload"),
+		})
+		return
+	}
+
+	db, err := config.ConnectMySQL()
+	if err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "Failed to connect to MySQL",
+			Err: err,
+		})
+		return
+	}
+
+	if err := createTherapistInDB(db, therapistRequest); err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "Failed to create therapist",
+			Err: err,
+		})
+		return
+	}
+
+	util.CallSuccessOK(c, util.APISuccessParams{
+		Msg:  "Therapist created",
+		Data: nil,
+	})
+}
+
+func UpdateTherapist(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		util.CallUserError(c, util.APIErrorParams{
+			Msg: "Missing therapist ID",
+			Err: fmt.Errorf("therapist ID is required"),
+		})
+		return
+	}
+
+	therapist := model.Therapist{}
+	if err := c.ShouldBindJSON(&therapist); err != nil {	
+		util.CallUserError(c, util.APIErrorParams{
+			Msg: "Invalid request body",
+			Err: err,
+		})
+		return
+	}
+
+	db, err := config.ConnectMySQL()
+	if err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "Failed to connect to MySQL",
+			Err: err,
+		})
+		return
+	}
+
+	var existingTherapist model.Therapist
+	if err := db.First(&existingTherapist, id).Error; err != nil {
+		util.CallUserError(c, util.APIErrorParams{
+			Msg: "Therapist not found",
+			Err: err,
+		})
+		return
+	}
+
+	if err := db.Model(&existingTherapist).Updates(therapist).Error; err != nil {
+		util.CallServerError(c, util.APIErrorParams{
+			Msg: "Failed to update therapist",
+			Err: err,
+		})
+		return
+	}
+
+	util.CallSuccessOK(c, util.APISuccessParams{
+		Msg:  "Therapist updated",
+		Data: nil,
 	})
 }
