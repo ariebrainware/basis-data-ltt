6641ac9c3c3f1c56870a9967212507e0bafb55d2
Arie Brainware <publicu001@protonmail.com>
2026-06-24 09:08:11 +0700

    refactor: reformat struct tag indentation in Transaction model

--- model/transaction.go
+++ model/transaction.go
@@ -13,12 +13,12 @@ type TransactionItem struct {
 // @Description Transaction information for a treatment
 type Transaction struct {
 	gorm.Model
-	TreatmentID   uint   `json:"treatment_id" gorm:"not null;index" example:"1"`
-	TherapistID   uint   `json:"therapist_id" gorm:"not null;index" example:"1"`
-	Amount        int64  `json:"amount" gorm:"not null" example:"50000"`
-	Remarks       string `json:"remarks" example:"Urgent handling fee"`
-	PaymentMethod string `json:"payment_method" example:"cash"`
-	PaymentStatus string `json:"payment_status" gorm:"default:'unpaid'" example:"unpaid"`
+	TreatmentID   uint              `json:"treatment_id" gorm:"not null;index" example:"1"`
+	TherapistID   uint              `json:"therapist_id" gorm:"not null;index" example:"1"`
+	Amount        int64             `json:"amount" gorm:"not null" example:"50000"`
+	Remarks       string            `json:"remarks" example:"Urgent handling fee"`
+	PaymentMethod string            `json:"payment_method" example:"cash"`
+	PaymentStatus string            `json:"payment_status" gorm:"default:'unpaid'" example:"unpaid"`
 	Items         []TransactionItem `json:"items,omitempty" gorm:"serializer:json;type:json"`
 }
 
